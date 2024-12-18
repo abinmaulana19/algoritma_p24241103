5b7c30ea44265702e1b87bde39a1ee17118e8f5f
abinmaulana19 <ybinnsama@gmail.com>
2024-12-18 12:50:25 +0800

    Add files via upload

---
+++ modul 5 algoritma/11-latihanpercabangan/main.py
@@ -0,0 +1,8 @@
+totalbelanja = float(input("masukan nama anda ="))
+if totalbelanja > 500000:
+    # aksi 1
+    print("selamat dapat bonus mouse dan keyboard")
+else:
+    # aksi 2
+    print("dapat gantungan kunci")
+print('terimah kasih')
