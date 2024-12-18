954272f5503381d0ce688af7b700c9cf452d30f4
abinmaulana19 <ybinnsama@gmail.com>
2024-12-18 12:56:31 +0800

    Delete modul 5 algoritma directory

--- modul 5 algoritma/11-latihanpercabangan/main.py
+++
@@ -1,8 +0,0 @@
-totalbelanja = float(input("masukan nama anda ="))
-if totalbelanja > 500000:
-    # aksi 1
-    print("selamat dapat bonus mouse dan keyboard")
-else:
-    # aksi 2
-    print("dapat gantungan kunci")
-print('terimah kasih')
